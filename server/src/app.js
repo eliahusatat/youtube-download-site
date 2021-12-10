88060a152fc60f2fe9a94e340f9de1d99d52cd56
eliahusatat <eliahusatat@gmail.com>
2021-12-10 08:07:31 +0200

    running server

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,20 @@
 // "start": "nodemon src/app.js --exec  \"npm run lint && node\"",
 // "lint": "eslint src/**/*.js"
-console.log("hello");
+console.log("server start..."); 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req,res) => {
+    res.send({
+        message : 'hello world'
+    })
+});
+app.listen(8081);
